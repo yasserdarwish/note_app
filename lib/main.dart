555c67e066acfe797291236476b43506ce40d62f
Yasser <mr.yasser82@gmail.com>
2023-07-17 05:00:23 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
