4a958e5326859791929f2ce838678a24c71ab69b
Yasser <mr.yasser82@gmail.com>
2023-07-30 01:30:59 +0300

    create note item

--- lib/views/widgets/custom_search_icon.dart
+++ lib/views/widgets/custom_search_icon.dart
@@ -6,15 +6,16 @@ class CustomSearchIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 46,
-      width: 46,
-      decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.05),
-          borderRadius: BorderRadius.circular(16)),
-      child: const Icon(
-        Icons.search,
-        size: 28,
-      ),
-    );
+        height: 46,
+        width: 46,
+        decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.05),
+            borderRadius: BorderRadius.circular(16)),
+        child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              size: 28,
+            )));
   }
 }
