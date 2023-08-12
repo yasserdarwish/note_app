58b43b85abaf0825d18bdd90bcd9d739222fdf93
Yasser <mr.yasser82@gmail.com>
2023-08-12 23:49:42 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String content;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.content,
+      required this.date,
+      required this.color});
+}
