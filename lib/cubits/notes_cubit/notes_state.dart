c20a39fffb1828f3b6180301a2192371fed50457
Yasser <mr.yasser82@gmail.com>
2023-09-07 02:37:34 +0300

    crate notes cubit and add states

---
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,19 @@
+part of 'notes_cubit.dart';
+
+@immutable
+sealed class NotesState {}
+
+final class NotesInitial extends NotesState {}
+
+final class NotesLoading extends NotesState {}
+
+final class NotesSuccess extends NotesState {
+  final NoteModel notes;
+
+  NotesSuccess(this.notes);
+}
+
+final class NotesFailure extends NotesState {
+  final String errMessage;
+  NotesFailure(this.errMessage);
+}
