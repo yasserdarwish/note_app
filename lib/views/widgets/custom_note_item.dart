7957b669d004282c7cb336412ad83e156cdcdeba
Yasser <mr.yasser82@gmail.com>
2023-09-07 04:55:20 +0300

    delete note done

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -36,7 +36,9 @@ class NoteItem extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      note.delete();
+                    },
                     icon: const Icon(
                       FontAwesomeIcons.trash,
                       color: Colors.black,
